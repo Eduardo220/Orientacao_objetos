d3fbb87ea19f25e81c9659eba8bb266d6abe4c15
Eduardo220 <eduardo.weissheimer22@gmail.com>
2025-04-17 01:01:40 -0300

    atualização no html, incluindo cadastro

--- src/Web/Login/login.php
+++ src/Web/Login/login.php
@@ -8,19 +8,4 @@ function validarEmail(string $email): bool {
     }
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['username'] ?? '';
-    $senha = $_POST['password'] ?? '';
-    
-    try {
-        if (validarEmail($email)) {
-            // Aqui você poderia validar o login com banco de dados
-            $mensagem = "Login efetuado com sucesso!";
-            $classe = "sucesso";
-        }
-    } catch (InvalidArgumentException $e) {
-        $mensagem = $e->getMessage();
-        $classe = "erro";
-    }
-}
-?>
+
